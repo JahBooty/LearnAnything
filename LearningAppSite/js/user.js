4910b51ed6a861a2912723c88565ecfe44dcf0cd
jacob booten <jacob@fcim.org>
2023-04-14 15:48:26 -0400

    add score and points bbutton

--- LearningAppSite/js/user.js
+++ LearningAppSite/js/user.js
@@ -11,9 +11,35 @@ console.log(userInfo.userName);
 
 console.log(userInfo.passWord);
 
+
 // put user information on the page
 
 document.getElementById("userName").innerHTML = userInfo.userName;
 
 document.getElementById("userPass").innerHTML = userInfo.passWord;
+
+
+// combine userInfo and userScore
+
+var userScore = 0;
+
+const userInfoWithScore = [
+    ['userName', (userInfo.userName)],
+    ['userPassword', (userInfo.passWord)],
+    ['userScore', (userScore)]
+]
+
+console.log(userInfoWithScore);
+
+// put userScore on the page
+
+document.getElementById("scoreHere").innerHTML = userScore;
+
+// add point to userScore
+
+function addPoint() {
+    userScore++;
+    document.getElementById("scoreHere").innerHTML = userScore;
+}
+
 
